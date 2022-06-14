911b416a431baa42045375fca5c19677eaab01f4
armyofkittens <ryan.knauff@gmail.com>
2022-06-14 16:36:35 -0600

    updating through 13.5.2

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,38 +1,40 @@
 // Add console.log to check to see if our code is working.
-
 console.log("working");
 
-// Create the map object with a center and zoom level.
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+// // Grabbing our GeoJSON data.
+// // GeoJSON reverse lat-long to long-lat
+L.geoJson(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+   console.log(layer);
+   layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa +
+   "</h3><hr><p>" + " Airport Name: " + feature.properties.name + "</p>");
+   }
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-  }).addTo(map);
-
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-     radius: city.population/100000,
-     color: "orange",
-     fillColor: '#FFA500'
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
